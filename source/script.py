f444e4912a93aee8cde3d40e368bfea292c313fd
phzamora <p.hzamora@icloud.com>
2024-04-19 12:27:39 +0200

    refactor: script.py

--- source/script.py
+++ source/script.py
@@ -13,24 +13,22 @@ class Size(NamedTuple):
 
 
 class MosaicGenerator:
-    mini_pic_res = Size(40, 40)
-
     def __init__(
         self,
         main_picture: Path | str,
         picture_suite: Path | str,
         target_res: Size,
         export_path: Path | str,
+        mini_pic_res: Size = None,
     ) -> None:
         self._main_picture: Path | str = main_picture
         self._picture_suite: Path | str = picture_suite
         self._target_res: Size = target_res
         self._export_path: Path | str = Path(export_path)
+        self.mini_pic_res: Size = mini_pic_res
 
     @staticmethod
-    def show_image_from_arr(
-        arr1: np.ndarray, arr2: Optional[np.ndarray] = None
-    ) -> None:
+    def show_image_from_arr(arr1: np.ndarray, arr2: Optional[np.ndarray] = None) -> None:
         _, axs = plt.subplots(1, 2, figsize=(10, 5))
 
         axs[0].imshow(Image.fromarray(arr1))
@@ -51,10 +49,9 @@ class MosaicGenerator:
         return im_arr
 
     @classmethod
-    def resize_image(cls, img: Image, shape: Size = None) -> np.ndarray:
+    def resize_image(cls, img: Image, shape: Size) -> np.ndarray:
         """Takes an image and resizes to a given size (width, height) as passed to the size parameter"""
 
-        shape = shape if shape is not None else cls.mini_pic_res
         resz_img = ImageOps.fit(
             img,
             shape,
@@ -64,30 +61,24 @@ class MosaicGenerator:
         return np.array(resz_img)
 
     def generate(self):
-        face_im_arr = self.load_main_picture()
-
-        height = face_im_arr.shape[0]
-        width = face_im_arr.shape[1]
-
-        mos_template: np.ndarray = face_im_arr[
-            :: (height // self._target_res.height), :: (width // self._target_res.width)
-        ]
-        # self.show_image_from_arr(mos_template)
-        # self.show_image_from_arr(face_im_arr, mos_template)
-
-        images = self.crop_mosaic_image()
-
-        # self.show_image_from_arr(images[4])
-
-        images_array: np.ndarray = np.asarray(images)
-
-        image_values = np.apply_over_axes(np.mean, images_array, [1, 2]).reshape(
-            len(images), 3
+        img_list = self.create_image_list()
+        canvas = Image.new(
+            "RGB",
+            (
+                self.mini_pic_res.width * self._target_res.width,
+                self.mini_pic_res.height * self._target_res.height,
+            ),
         )
 
-        tree = spatial.KDTree(image_values)
+        self.paste_pics_in_canvas(img_list, canvas)
 
-        image_idx = np.zeros(self._target_res, dtype=np.uint32)
+        self.save(canvas)
+
+    def paste_pics_in_canvas(self, img_list: np.ndarray, canvas: Image.Image) -> None:
+        images_array: np.ndarray = np.asarray(img_list)
+        image_values = np.apply_over_axes(np.mean, images_array, [1, 2]).reshape(len(img_list), 3)
+        mos_template: np.ndarray = self.create_mos_template()
+        tree = spatial.KDTree(image_values)
 
         for i in range(self._target_res.height):
             for j in range(self._target_res.width):
@@ -95,40 +86,45 @@ class MosaicGenerator:
 
                 match = tree.query(template, k=40)
                 pick = random.randint(0, 39)
-                image_idx[j, i] = match[1][pick]
 
-        canvas = Image.new(
-            "RGB",
-            (
-                self.mini_pic_res.height * self._target_res.height,
-                self.mini_pic_res.width * self._target_res.width,
-            ),
-        )
-
-        for i in range(self._target_res.height):
-            for j in range(self._target_res.width):
-                arr = images[image_idx[j, i]]
+                arr = img_list[match[1][pick]]
                 x, y = j * self.mini_pic_res.width, i * self.mini_pic_res.height
                 im = Image.fromarray(arr)
                 canvas.paste(im, (x, y))
+        return None
 
-        canvas.save(self._export_path)
+    def create_mos_template(self):
+        face_im_arr = self.load_image(self._main_picture)
 
-    def crop_mosaic_image(self) -> list[np.ndarray]:
+        MAX_HEIGHT = face_im_arr.shape[0]
+        MAX_WIDTH = face_im_arr.shape[1]
+
+        return face_im_arr[:: (MAX_HEIGHT // self._target_res.height), :: (MAX_WIDTH // self._target_res.width)]
+
+    def create_image_list(self) -> list[np.ndarray]:
         images: list[np.ndarray] = []
 
         lista = list(self._picture_suite.rglob("*.png"))
         for file in lista:
             image_arr: np.ndarray = self.load_image(file)
 
-            if image_arr.ndim == 3:
+            if self.mini_pic_res is None:
+                self.mini_pic_res = Size(*image_arr.shape[:-1])
+
+            if image_arr.ndim == 3 and image_arr.shape[-1] == 3:
                 limg = Image.fromarray(image_arr)
-                resize_img = self.resize_image(limg)
+                resize_img = self.resize_image(limg, self.mini_pic_res)
                 images.append(resize_img)
             else:
-                self.show_image_from_arr(image_arr)
+                pass
+                # self.show_image_from_arr(image_arr)
         return images
 
-    def load_main_picture(self) -> np.ndarray:
-        face_im_arr = self.load_image(self._main_picture)
-        return face_im_arr
+    def save(self, canvas: Image.Image) -> None:
+        export_name = (
+            f"{self._target_res.height}x{self._target_res.height}-"
+            f"min_res {self.mini_pic_res.width}x{self.mini_pic_res.height}-"
+            "Export.jpg"
+        )
+        canvas.save(self._export_path / export_name)
+        return None
