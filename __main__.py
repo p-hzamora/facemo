88352b80ec1f72440cc0523b3204c473f52502d1
phzamora <p.hzamora@icloud.com>
2024-04-22 00:34:11 +0200

    fix: __main__.py

--- __main__.py
+++ __main__.py
@@ -1,18 +1,22 @@
 from pathlib import Path
 from source.script import MosaicGenerator, Size
 
+
 if __name__ == "__main__":
+    main_pic = Path(__file__).parent / "Images" / "face-image-marina.png"
+    pic_suite = Path(__file__).parent.parent / "720pixels"
+    export = Path(__file__).parent / "Images" / "Exports"
 
-    main_pic = Path(__file__).parent / "Images" / "cuadrada_mano.png"
-    pic_suite = Path(__file__).parent / "Images" / "Mosaic-Images-real"
-    export = Path(__file__).parent / "Images" / "Exports" / "exportacion_marina.jpg"
-
-    MosaicGenerator(
+    mosaico = MosaicGenerator(
         main_picture=main_pic,
         picture_suite=pic_suite,
-        target_res=Size(150, 100),
+        main_transparency=.9,
+        target_res=Size(63,94),
         export_path=export,
-        mini_pic_res=Size(700,700)
-    ).generate()
+        mini_pic_res=Size(40, 40),
+    )
+    mosaico.generate()
+    mosaico.canvas.show()
+    mosaico.save()
 
     print("Mosaico realizado con exito")
