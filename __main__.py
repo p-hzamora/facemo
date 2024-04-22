f1ef3debee287fec576362995bdbefb4e028edcb
phzamora <p.hzamora@icloud.com>
2024-04-22 02:12:45 +0200

    update: __main__.py

--- __main__.py
+++ __main__.py
@@ -3,17 +3,17 @@ from source.script import MosaicGenerator, Size
 
 
 if __name__ == "__main__":
-    main_pic = Path(__file__).parent / "Images" / "face-image-marina.png"
+    main_pic = Path(__file__).parent / "Images" / "segunda opcion.jpg"
     pic_suite = Path(__file__).parent.parent / "720pixels"
     export = Path(__file__).parent / "Images" / "Exports"
 
     mosaico = MosaicGenerator(
         main_picture=main_pic,
         picture_suite=pic_suite,
-        main_transparency=.9,
-        target_res=Size(63,94),
+        canvas_width=56,
+        # target_res=Size(63,94),
         export_path=export,
-        mini_pic_res=Size(40, 40),
+        mini_pic_res=Size(350, 350),
     )
     mosaico.generate()
     mosaico.canvas.show()
